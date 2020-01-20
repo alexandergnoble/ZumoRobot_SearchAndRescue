aa8aa75ab8e8f94d3942b0142c5c128a9af65803
alexandernoble <49210041+alexandernoble@users.noreply.github.com>
2020-01-20 02:36:16 +0000

    Initial file structure upload
    Libraries, sketch for Arduino and base WPF uploaded.

---
+++ ZumoGUI/ZumoGUI/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO.Ports;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZumoGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SerialPort mySerialPort = new SerialPort("COM5", 9600);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnW_Click(object sender, RoutedEventArgs e)
+        {
+
+             mySerialPort.Open();
+             mySerialPort.Write("W");
+             mySerialPort.Close();
+
+        }
+
+        private void btnA_Click(object sender, RoutedEventArgs e)
+        {
+            mySerialPort.Open();
+            mySerialPort.Write("A");
+            mySerialPort.Close();
+        }
+
+        private void btnS_Click(object sender, RoutedEventArgs e)
+        {
+            mySerialPort.Open();
+            mySerialPort.Write("S");
+            mySerialPort.Close();
+        }
+
+        private void btnD_Click(object sender, RoutedEventArgs e)
+        {
+            mySerialPort.Open();
+            mySerialPort.Write("D");
+            mySerialPort.Close();
+        }
+
+        private void output_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string test = output.Text;
+
+            switch (test)
+            {
+                case "w": case "W":
+                mySerialPort.Open();
+                mySerialPort.Write("W");
+                mySerialPort.Close();
+                output.Text = String.Empty;
+                break;
+
+                case "a": case "A":
+                mySerialPort.Open();
+                mySerialPort.Write("A");
+                mySerialPort.Close();
+                output.Text = String.Empty;
+                break;
+
+                case "s": case "S":
+                mySerialPort.Open();
+                mySerialPort.Write("S");
+                mySerialPort.Close();
+                output.Text = String.Empty;
+                break;
+
+                case "d": case "D":
+                mySerialPort.Open();
+                mySerialPort.Write("D");
+                mySerialPort.Close();
+                output.Text = String.Empty;
+                break;
+            }         
+        }
+    }
+}
---
+++ zumoSearchAndRescue/zumoSearchAndRescue.ino
@@ -0,0 +1,60 @@
+#include <Zumo32U4.h>
+
+// Zumo32U4ButtonA buttonA;
+// Documentation for the motors class:
+// http://pololu.github.io/zumo-32u4-arduino-library/class_zumo32_u4_motors.html
+Zumo32U4Motors motors;
+
+const uint16_t motorSpeed = 200;
+
+void setup() {
+  Serial1.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+
+  while (Serial1.available()>0) {
+    
+    int direction = Serial1.read();
+    Serial.print("Got message: ");
+    Serial.println(direction);
+    String name ="";
+
+    switch (direction){
+      case'w': case 'W':
+      name = "Forward";
+      motors.setSpeeds(motorSpeed, motorSpeed);
+      delay(250);
+      motors.setSpeeds(0, 0);
+      break;
+      
+      case'a': case 'A':
+      name = "Left";
+      motors.setSpeeds(-motorSpeed, motorSpeed);
+      delay(50);
+      motors.setSpeeds(0, 0);
+      break;
+      
+      case'd': case 'D':
+      name = "Right";
+      motors.setSpeeds(motorSpeed, -motorSpeed);
+      delay(50);
+      motors.setSpeeds(0, 0);
+      break;
+      
+      case's': case 'S':
+      name = "Backwards";
+      motors.setSpeeds(-motorSpeed, -motorSpeed);
+      delay(50);
+      motors.setSpeeds(0, 0);
+      break;
+
+      case'z': case 'Z':
+      name = "Stop";
+      motors.setSpeeds(0, 0);
+      break;
+    }
+ }
+}
